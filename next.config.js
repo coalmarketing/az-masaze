6f7305c2e77fedc4103ec8cd6fb2b98d1bce5302
Ondřej Krejčí <ondrakrejci83@seznam.cz>
2025-05-19 12:30:54 +0200

    o

--- next.config.js
+++ next.config.js
@@ -1,19 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['testadmin-masaze.matfix.cz', 'admin.az-masaze.cz'],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    formats: ['image/webp'],
+    minimumCacheTTL: 60,
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'testadmin-masaze.matfix.cz',
-        port: '',
-        pathname: '/images/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'admin.az-masaze.cz',
-        port: '',
-        pathname: '/images/**',
+        pathname: '/**',
       },
     ],
   },
